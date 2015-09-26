fecf7f6a256aa4076451482e169395aa6a83c99f
Dan Baumfeld <thethirddan@gmail.com>
2015-09-25 22:08:21 -0700

    bootstraped

--- app/components/inline-edit.js
+++ app/components/inline-edit.js
@@ -4,19 +4,18 @@ export default Ember.Component.extend({
   click() {
     //if isEditable is true just return
     if(this.get('isEditable')) {
-      return
+      return;
     }
 
-    this.toggleProperty('isEditable');
+    //this.toggleProperty('isEditable');
+    this.set('isEditable', true);
   },
   actions: {
     save(){
-      //this.toggleProperty('isEditable');
       this.set('isEditable', false);
       this.sendAction('action', this.get('model'));
-
-      this.isEditable = false;
-      console.log("this", this);
+      // stop propigation
+      return false;
     }
   },
   isEditable: false
--- app/routes/items/new.js
+++ app/routes/items/new.js
@@ -1,19 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('item');
+  },
+
   actions: {
     submit(item) {
-      console.log("itemgetname: ", item.get('name'));
-      let postitem = this.store.createRecord('item', {
-        name: item.get('name'),
-        description: item.get('description')
-      });
-
       item.save().then(()=> {
         this.transitionTo('items');
       });
     },
     cancel(){
+      this.modelFor('items.new').destroyRecord();
       this.transitionTo('items');
     }
   }
